ce7914bebda7f68d10aab8aee23ad167f670bafc
Loudi <loudimail@gmail.com>
2024-09-17 15:01:36 +0200

    cleaned up main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,20 +8,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-//        String apiKey = System.getenv("TMDB_API_KEY");
-//        String url = "https://api.themoviedb.org/3/movie/💥?api_key=#";
-//        String fullUrl = url.replace("#",apiKey);
-//        fullUrl = fullUrl.replace("💥","533535");
-//        System.out.println(apiKey);
-//        System.out.println(fullUrl);
-//
-//        String urlMulti = "https://api.themoviedb.org/3/discover/movie?api_key=#&with_original_language=da&region=DK&page=!";
-//        String fullUrlMulti = urlMulti.replace("#",apiKey);
-//        fullUrlMulti = fullUrlMulti.replace("!","1");
-//        System.out.println(fullUrlMulti);
-
         ApiReader apiReader = new ApiReader();
-//        apiReader.readMultiple(fullUrlMulti);
+
         List<MovieDTO> movies = apiReader.readMovieMultiple();
         movies.forEach(System.out::println);
         apiReader.apiCasts.forEach(System.out::println);
