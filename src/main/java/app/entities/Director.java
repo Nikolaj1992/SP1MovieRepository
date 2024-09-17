a3b448117d8ac5bcfb7b637301ea975d021d367f
NikolajF <cph-nf70@cphbusiness.dk>
2024-09-17 15:21:27 +0200

    This will need changing, but early entities made

--- src/main/java/app/entities/Director.java
+++ src/main/java/app/entities/Director.java
@@ -1,4 +1,27 @@
 package app.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "movie_director")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Director {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String job;
+
+    @ManyToMany
+    private List<Movie> movies;
+
 }
