d3c82999013aab051e85dd9298f0316cab6a86a0
Loudi <loudimail@gmail.com>
2024-09-17 15:31:26 +0200

    only takes danish movies from the 5 last years in constant link and now gives a giant list of movies and casts but is super slow

--- src/main/java/app/constants/LinkConstants.java
+++ src/main/java/app/constants/LinkConstants.java
@@ -8,7 +8,8 @@ public final class LinkConstants {
     // ! = page number (only MOVIE_MULTI_LINK)
 
     public static final String MOVIE_SINGLE_LINK = "https://api.themoviedb.org/3/movie/!?api_key=#";
-    public static final String MOVIE_MULTI_LINK = "https://api.themoviedb.org/3/discover/movie?api_key=#&with_original_language=da&region=DK&page=!";
+//    public static final String MOVIE_MULTI_LINK = "https://api.themoviedb.org/3/discover/movie?api_key=#&with_original_language=da&region=DK&page=!"; //without 5 year limit
+    public static final String MOVIE_MULTI_LINK = "https://api.themoviedb.org/3/discover/movie?api_key=#&with_original_language=da&region=DK&page=!&primary_release_date.gte=2019-01-01&primary_release_date.lte=2024-12-31";
     public static final String MOVIE_CAST_LINK = "https://api.themoviedb.org/3/movie/!/credits?api_key=#";
     public static final String PERSON_LINK = "https://api.themoviedb.org/3/person/!?api_key=#";
 }
