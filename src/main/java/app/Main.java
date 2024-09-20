820176e0a5f366a6c2a196f7d23d844c70e7ef07
Loudi <loudimail@gmail.com>
2024-09-20 15:42:50 +0200

    get lowest, highest and most popular movies methods works and are current tested inside of main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,7 +2,9 @@ package app;
 
 import app.config.HibernateConfig;
 import app.dao.ApiDAO;
+import app.dao.MovieDAO;
 import app.entities.Movie;
+import app.services.MovieService;
 import app.services.api.ApiReader;
 import jakarta.persistence.EntityManagerFactory;
 
@@ -16,7 +18,7 @@ public class Main {
         apiReader.readMovieMultiple();
 //        apiReader.apiMovies.forEach(System.out::println);
 //        apiReader.apiCredits.forEach(System.out::println);
-        System.out.println("Amount of movies: " + apiReader.apiMovies.size());
+//        System.out.println("Amount of movies: " + apiReader.apiMovies.size());
 //        System.out.println("Amount of credits: " + apiReader.apiCredits.size());
 //        apiReader.apiActors.forEach(System.out::println);
 //        apiReader.apiDirectors.forEach(System.out::println);
@@ -32,5 +34,11 @@ public class Main {
         List<Movie> movies = apiReader.apiMovies.stream().map(mDTO -> new Movie(mDTO)).toList();
         ApiDAO apiDAO = ApiDAO.getInstance(emf);
         apiDAO.persistAll(movies);
+
+        MovieDAO movieDAO = MovieDAO.getInstance(emf);
+        MovieService movieService = new MovieService(movieDAO);
+        movieService.getHigestRatedMovies(10).forEach(movieDTO -> System.out.println("Highest: " + movieDTO.getTitle() + " - " + movieDTO.getVoteAverage()));
+        movieService.getLowestRatedMovies(10).forEach(movieDTO -> System.out.println("Lowest: " + movieDTO.getTitle() + " - " +  movieDTO.getVoteAverage()));
+        movieService.getMostPopularMovies(10).forEach(movieDTO -> System.out.println("Popular: " + movieDTO.getTitle() + " - " +  movieDTO.getVoteAverage()+ " - " +  movieDTO.getVoteCount()));
     }
 }
--- src/main/java/app/services/MovieService.java
+++ src/main/java/app/services/MovieService.java
@@ -11,10 +11,11 @@ import java.util.stream.Collectors;
 public class MovieService {
 
     private MovieDAO movieDAO;
-    private List<MovieDTO> movieList= movieDAO.findAll();
+    private List<MovieDTO> movieList = new ArrayList<>();
 
     public MovieService(MovieDAO movieDAO) {
         this.movieDAO = movieDAO;
+        this.movieList = this.movieDAO.findAll();
     }
 
     public List<MovieDTO> searchForMovieByTitle(String title) {
@@ -28,22 +29,18 @@ public class MovieService {
     }
 
     public List<MovieDTO> getHigestRatedMovies(int amountOfMovies) {
-        List<MovieDTO> results = new ArrayList<>();
-        results = movieList.stream().sorted(Comparator.comparing(movieDTO -> movieDTO.getVoteAverage())).limit(amountOfMovies).collect(Collectors.toList());
-        return results;
+        return movieList.stream().sorted(Comparator.comparingDouble(MovieDTO::getVoteAverage).reversed()).limit(amountOfMovies).collect(Collectors.toList());
     }
 
     public List<MovieDTO> getLowestRatedMovies(int amountOfMovies) {
-        List<MovieDTO> results = new ArrayList<>();
-        results = movieList.stream().sorted(Comparator.comparingDouble(MovieDTO::getVoteAverage).reversed()).limit(amountOfMovies).collect(Collectors.toList());
-        return results;
+        return movieList.stream().sorted(Comparator.comparingDouble(MovieDTO::getVoteAverage)).limit(amountOfMovies).collect(Collectors.toList());
     }
 
     public List<MovieDTO> getMostPopularMovies(int amountOfMovies) {
         List<MovieDTO> results = new ArrayList<>();
-        for (MovieDTO movieDTO : movieList) {
-
-        }
+        results = movieList.stream().sorted(Comparator.comparing(MovieDTO::getVoteCount).reversed()).limit(amountOfMovies).collect(Collectors.toList());
+        results = results.stream().sorted(Comparator.comparingDouble(MovieDTO::getVoteAverage).reversed()).collect(Collectors.toList());
+        // we can remove the second stream and only sort be voteCount if we want
         return results;
     }
 
